471c185927896b9354840c29e1cc2527e55dc7d9
Philip Hale <philip@pghale.com>
2015-02-16 14:38:49 +0000

    create_report now abstracts the syntax for list of fields.
    In other words, instead of supplying the whole payload you now just have
    to list the field key/value pairs.
    
    In addition, these list of fields is no longer required for reports
    that have no fields.

--- lib/accela/apis/report_api.rb
+++ lib/accela/apis/report_api.rb
@@ -2,12 +2,13 @@ module Accela
   class ReportAPI < APIGroup
     as_class_method :create_report
 
-    def create_report(id: nil, module_name: nil, payload: nil)
-      unless id && module_name && payload
-        raise BadRequestError.new("Must supply ID and module_name and payload")
+    def create_report(id: nil, module_name: nil, fields: {})
+      unless id && module_name
+        raise BadRequestError.new("Must supply ID and module_name")
       end
       query = { module: module_name}
-      Accela::V4::CreateReport.result(id, query, payload)
+      payload = { parameters: fields }
+      Accela::V4::CreateReport.call(id, query, payload)
     end
   end
 end
