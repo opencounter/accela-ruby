5ec84526a89e71616235d418b023447c90c315c8
Philip Hale <philip@pghale.com>
2015-02-16 14:37:35 +0000

    create_report now working, returns a StringIO wrapper binary.
    Notes:
    
    * Since reports are not always PDFs, this is very generic.

--- lib/accela/api/v4/create_report.rb
+++ lib/accela/api/v4/create_report.rb
@@ -3,7 +3,12 @@ module Accela
     class CreateReport < Base
 
       def call(id, query, payload)
-        post("reports/#{id}/", :access_token, query, payload)
+        headers = {
+          'Content-Type' => 'application/json',
+          'Accept' => '*/*'
+        }
+        resp = post("reports/#{id}/", :access_token, query, payload, headers)
+        StringIO.new resp
       end
 
     end
